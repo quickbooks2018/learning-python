76179d79bcb0987c756209a61ddf0e555688e7cd
Muhammad Asim <asim@arbisoft.com>
2021-09-28 08:59:48 +0500

    Learning Python

---
+++ sys-admins/python-scripts/user-input-in-python.py
@@ -0,0 +1,22 @@
+# name=input("What is your name? ")
+
+# age=input("What is your age? ")
+
+# message_name="Thanks your name is {}, right".format(name)
+#
+# message_age="Thanks you age is {}".format(age)
+#
+# print(message_name)
+#
+# print(message_age)
+
+
+# You can also use Substitute
+
+name = input("What is your name? ")
+
+age = int(input("What is your age? "))
+
+print("Your name is %s and your age %s" % (name, age))
+
+print("Half of your age is %s" % (age / 2))
