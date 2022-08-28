a34b46efeb659fac12d3c089cea84de24c8687e2
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-08-28 11:10:18 +0500

    variable in python with f or format

---
+++ sys-admins/variables-in-python-with-f.py
@@ -0,0 +1,10 @@
+n1 = int('10')
+
+n2 = int(20)
+
+sumofn1n2 = n1 + n2
+
+#print('Value of n1 is {} and value n2 is {} and sum is {}'.format(n1, n2, sumofn1n2))
+
+
+print(f'Value of n1 is {n1} & value of n2 is {n2} and sum is {sumofn1n2}')
