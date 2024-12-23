389caa3be9ee08dd2d477d775c6e21c6a1f30f49
Muhammad Asim <quickbooks2018@gmail.com>
2024-12-23 09:39:24 +0500

    list insert

---
+++ list/list-insert.py
@@ -0,0 +1,13 @@
+# index   0        1      2       3      4
+# Note: list does not remove any duplicates and it maintains the order of the elements
+# Note: list is mutable means you can change the elements of the list
+
+list = ['asim', 'qasim', 'taha', 'zoha', 'hala', 'duwa', 'fiza']
+
+list.insert(0, 'Abujan')
+list.insert(1,'Amijan')
+
+for i in list:
+    print(i)
+
+print('end of program')
